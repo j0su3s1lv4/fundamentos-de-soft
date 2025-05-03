e60c409a4bef94c53a8c410993146bc9c82b66fc
j0su3s1lv4 <fenopai057@gmail.com>
2025-05-03 16:48:55 -0300

    Add files via upload

---
+++ crc/script.js
@@ -0,0 +1,11 @@
+let botao=document.createElement("button");
+   botao.innerText="Se liga nesse motivacional!";
+     document.body.appendChild(botao);
+      botao.onclick=function(){
+       
+      botao.style.color="black";
+    botao.style.fontSize="40px";  
+botao.innerText="Não sabendo que era impossivel, foi la e soube!";
+}
+
+/*sempre uma  uma motivacional, ngm quer ler isso po*/
